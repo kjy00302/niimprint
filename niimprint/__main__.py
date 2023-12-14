0e7a297f5b59559242f03eee53277e48b388b273
kjy00302 <kjy00302@gmail.com>
2023-12-15 03:01:31 +0900

    Remove image check

--- niimprint/__main__.py
+++ niimprint/__main__.py
@@ -13,18 +13,11 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description="Niimbot printer client")
     parser.add_argument('-a', '--address', required=True, help="MAC address of target device")
-    parser.add_argument('--no-check', action='store_true', help="Skips image check")
     parser.add_argument('-d', '--density', type=int, default=2, help="Printer density (1~3)")
     parser.add_argument('-t', '--type', type=int, default=1, help="Label type (1~3)")
     parser.add_argument('-n', '--quantity', type=int, default=1, help="Number of copies")
     parser.add_argument('image', help="PIL supported image file")
     args = parser.parse_args()
-
-    img = Image.open(args.image)
-    if img.width / img.height > 1:
-        # rotate clockwise 90deg, upper line (left line) prints first.
-        img = img.transpose(Image.ROTATE_270)
-    assert args.no_check or (img.width == 96 and img.height < 600)
 
     printer = printerclient.PrinterClient(args.address)
     printer.set_label_type(args.type)
